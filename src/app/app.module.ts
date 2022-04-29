9f02c1eac43f068f48b8a6abb7498adb3026208f
dannotdead <daniil.koka2000@gmail.com>
2022-04-29 19:13:29 +0700

    fix sctructure of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MapComponent } from '../map/map.component';
-import { MenuComponent } from '../menu/menu.component';
+import { MapComponent } from './map/map.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
---
+++ src/app/map-control.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MapControlService {
+
+  constructor() { }
+  // object: map
+  // method: clear map
+  // при инициализации присваиваем туда карту в сервис и управляем ей отсюда
+
+}
